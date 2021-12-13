412019481a09a84a52721ac9d9e555e5effc71bc
Woa <0903wzy@gmail.com>
2021-12-13 20:55:56 +0800

    Lab: xv6 lazy page allocation: Lazy allocation

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -46,8 +46,8 @@ sys_sbrk(void)
 
   if(argint(0, &n) < 0)
     return -1;
-  myproc()->sz += n;
   addr = myproc()->sz;
+  myproc()->sz += n;
   // if(growproc(n) < 0)
   //   return -1;
   return addr;
