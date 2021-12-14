78a80fd385b84d517880eb888c6aa6dc57bd1ed8
Woa <0903wzy@gmail.com>
2021-12-14 12:09:04 +0800

    Lab: xv6 lazy page allocation: Lazytests and Usertests

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -46,10 +46,19 @@ sys_sbrk(void)
 
   if(argint(0, &n) < 0)
     return -1;
-  addr = myproc()->sz;
-  myproc()->sz += n;
-  // if(growproc(n) < 0)
-  //   return -1;
+
+  struct proc* p = myproc();
+  addr = p->sz;
+
+  if(n > 0) {
+    p->sz += n;
+    // if(growproc(n) < 0)
+    //   return -1;
+  } else if(p->sz + n > 0) {
+    p->sz = uvmdealloc(p->pagetable, p->sz, p->sz + n);
+  } else {
+    return -1;
+  }
   return addr;
 }
 
